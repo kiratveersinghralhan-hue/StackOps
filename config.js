b7b99b3832958c1485b2f1df2c78f45a44e2c1ae
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-05-03 14:09:04 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.STACKOPS_CONFIG = {
   SUPABASE_URL: 'https://ffagzruaerzftqwmnbmp.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmYWd6cnVhZXJ6ZnRxd21uYm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNTYyMTcsImV4cCI6MjA5MDczMjIxN30.H_bI5O0YLMOjvnyGyi6HIq1scBHydRebB3lx7nJkVzc',
-  RAZORPAY_KEY_ID: 'rzp_test_YOUR_KEY_ID',
+  RAZORPAY_KEY_ID: 'rzp_live_SkpHJhAcRCNvzO',
   ADMIN_EMAILS: ['kiratveersinghralhan@gmail.com','qq299629@gmail.com'],
   DEFAULT_LANGUAGE: 'en',
   STORAGE_BUCKETS: {
