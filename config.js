4b3dcd6962fabd30066ebcf7d498e896a3ec82a3
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-05-04 00:05:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.STACKOPS_CONFIG = {
+  SUPABASE_URL: 'https://ffagzruaerzftqwmnbmp.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmYWd6cnVhZXJ6ZnRxd21uYm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNTYyMTcsImV4cCI6MjA5MDczMjIxN30.H_bI5O0YLMOjvnyGyi6HIq1scBHydRebB3lx7nJkVzc',
+  ADMIN_EMAILS: ['kiratveersinghralhan@gmail.com','qq299629@gmail.com'],
+  UPI_ID: 'ralhanx@ptaxis',
+  UPI_QR: 'upi.jpeg',
+  COMMISSION_PERCENT: 15,
+  BRAND: 'StackOps'
+};
