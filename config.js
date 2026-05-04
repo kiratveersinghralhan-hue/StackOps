383bc7126dae4e47a945a4ce8f2a5d81c6f20cd8
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-05-04 10:39:40 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.STACKOPS_CONFIG = {
   SUPABASE_URL: 'https://ffagzruaerzftqwmnbmp.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6ImZmYWd6cnVhZXJ6ZnRxd21uYm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNTYyMTcsImV4cCI6MjA5MDczMjIxN30.H_bI5O0YLMOjvnyGyi6HIq1scBHydRebB3lx7nJkVzc',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmYWd6cnVhZXJ6ZnRxd21uYm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNTYyMTcsImV4cCI6MjA5MDczMjIxN30.H_bI5O0YLMOjvnyGyi6HIq1scBHydRebB3lx7nJkVzc',
   ADMIN_EMAILS: ['kiratveersinghralhan@gmail.com','qq299629@gmail.com'],
   MANUAL_UPI_ID: 'ralhanx@ptaxis',
   MANUAL_UPI_QR_URL: 'upi.jpeg',
